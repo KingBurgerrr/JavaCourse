e412a232bc451078c7f6b963f16a266698d4bb45
KingBurgerrr <king.burherh1n1@gmail.com>
2020-08-06 19:01:44 +0300

    Lesson 7 home work part 1/abstract classes

---
+++ Lesson 7/src/main/java/abstracta/Cat.java
@@ -0,0 +1,8 @@
+package abstracta;
+
+public class Cat extends Pet {
+    @Override
+    public void voice() {
+        System.out.println("Мяу-Мяу");
+    }
+}
---
+++ Lesson 7/src/main/java/abstracta/Main.java
@@ -0,0 +1,12 @@
+package abstracta;
+
+public class Main {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        Cat cat = new Cat();
+        Cow cow = new Cow();
+        dog.voice();
+        cat.voice();
+        cow.voice();
+    }
+}
