9ffdccb14ca5e1462278e606129e7c2ad5934083
KingBurgerrr <king.burherh1n1@gmail.com>
2020-08-08 15:52:28 +0300

    Lesson 6 Part 4 Amphibias

---
+++ Lesson 7/src/main/java/Lesson6/part4/Amphibia.java
@@ -0,0 +1,16 @@
+package Lesson6.part4;
+
+public class Amphibia {
+    private String eat;
+    private String sleep;
+    private String swim;
+    private String walk;
+
+    Amphibia(String eat, String sleep, String swim, String walk){
+        //System.out.println(" Чи ість: " + eat + " Чи спить? " + sleep + " Чи плаває? " + swim + " Чи гуляє? " + walk);
+        this.eat = eat;
+        this.sleep = sleep;
+        this.swim = swim;
+        this.walk = walk;
+    }
+}
