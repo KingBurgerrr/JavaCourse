56fb52f62124df256aad44f12f3141600edd1ca7
KingBurgerrr <king.burherh1n1@gmail.com>
2020-08-08 15:57:01 +0300

    Lesson 7 fixed!

--- com/lgs/lab/interface1/com/lgs/lab/interface2/Main.java
+++ com/lgs/lab/interface1/com/lgs/lab/interface2/Main.java
@@ -6,6 +6,6 @@ public class Main {
     public static void main(String[] args) {
         MyCalculator mc = new MyCalculator();
 
-        System.out.println("Додавання: " + mc.plus(10,5) + "\n" + "Віднімання: + " mc.minus(10,5) + "\n" + mc.divide(10,5) + "\n" + mc.multiply(10, 5));
+        System.out.println("Додавання: " + mc.plus(10,5) + "\n" + "Віднімання: + " mc.minus(10,5) + "\n" +  "Діління: " + mc.divide(10,5) + "Множення: " + "\n" + mc.multiply(10, 5));
     }
 }
