833c37885e45d079a0e75cf06806b340c6f463ba
KingBurgerrr <king.burherh1n1@gmail.com>
2020-08-17 14:32:00 +0300

    Lesson 8 part 1

--- Lesson 8 16.08/Part 1/Season.java
+++ Lesson 8 16.08/Part 1/Season.java
@@ -1,13 +1,3 @@
 public enum Season {
     WINTER, SUMMER, SPRING, FALL
-//    String month;
-//    void setMonth(String month) {
-//        this.month = month;
-//    }
-//
-//    public String getMonth() {
-//        return month;
-//    }
-//    Season(String month) {
-//    }
 }
